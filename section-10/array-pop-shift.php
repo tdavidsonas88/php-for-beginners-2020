390144538df912127fd8561d342f0ec8a710aa55
Tadas <tdavidsonas88@gmail.com>
2020-08-14 19:14:49 +0300

    array_shift function

--- section-10/array-pop-shift.php
+++ section-10/array-pop-shift.php
@@ -7,3 +7,9 @@ $lastCity = array_pop($cities);
 echo $lastCity;
 echo "<br>";
 print_r($cities);
+
+echo "<br>";
+$firstCity = array_shift($cities);
+echo $firstCity;
+echo "<br>";
+print_r($cities);
